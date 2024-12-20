f368cf1ce55a9bb0aeeece01be14f238598d4b00
clarencethe3rd <clarencechan524@gmail.com>
2024-12-20 15:08:10 +0000

    Update circle.py

--- circle.py
+++ circle.py
@@ -1,44 +1,57 @@
 import pgzrun
 import random
+
 WIDTH = 3440
 HEIGHT = 1450
 GRAVITY = 2000.0
+
 class Circle():
-    def __init__(self,x,y,r):
+    def __init__(self, x, y, r):
         self.x = x
         self.y = y
         self.r = r
-        self.vx = 200
-        self.vy = 0
+        self.vx = random.randint(-200, 200)
+        self.vy = random.randint(-200, 200)
+    
     def display(self):
-        screen.draw.filled_circle((self.x,self.y),self.r, "blue")
+        screen.draw.filled_circle((self.x, self.y), self.r, "blue")
 
+balls = [
+    Circle(random.randint(100, WIDTH - 100), random.randint(100, HEIGHT - 100), random.randint(20, 50))
+    for _ in range(5)
+]
 
-ball = Circle(100,100,30)
 def draw():
     screen.clear()
     screen.fill("black")
-    ball.display()
-    
+    for ball in balls:
+        ball.display()
+
 def update(dt):
-    uy = ball.vy
-    ball.vy += GRAVITY*dt
-    ball.y+=(uy+ball.vy)*0.5*dt
-    ball.x += ball.vx*dt
-    if ball.y>HEIGHT-30:
-        ball.vy = ball.vy*-1 - 50
-    elif ball.y<0:
-        ball.vy = ball.vy*-1 - 50
-    if ball.x<0:
-        ball.vx = ball.vx*-1 - 50
-    elif ball.x>WIDTH-30:
-        ball.vx = ball.vx*-1 - 50
+    for ball in balls:
+        uy = ball.vy
+        ball.vy += GRAVITY * dt
+        ball.y += (uy + ball.vy) * 0.5 * dt
+        ball.x += ball.vx * dt
+
+        if ball.y > HEIGHT - ball.r:
+            ball.vy = -ball.vy - 50
+            ball.y = HEIGHT - ball.r
+        elif ball.y < ball.r:
+            ball.vy = -ball.vy - 50
+            ball.y = ball.r
+
+        if ball.x < ball.r:
+            ball.vx = -ball.vx - 50
+            ball.x = ball.r
+        elif ball.x > WIDTH - ball.r:
+            ball.vx = -ball.vx - 50
+            ball.x = WIDTH - ball.r
+
 def on_key_down(key):
     if key == keys.SPACE:
-        ball.vx = ball.vx + 1000
-        ball.vy = ball.vy + 1000
-    
+        for ball in balls:
+            ball.vx += 1000
+            ball.vy += 1000
+
 pgzrun.go()
-
-
-    
